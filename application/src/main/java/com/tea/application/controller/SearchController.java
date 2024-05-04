b7d60e91004c4da758a24b4411132329684ad5c9
sahilshah16 <sahil.pj.shah@gmail.com>
2024-05-04 19:25:51 +0100

    Search Results Working

--- application/src/main/java/com/tea/application/controller/SearchController.java
+++ application/src/main/java/com/tea/application/controller/SearchController.java
@@ -1,17 +1,14 @@
 package com.tea.application.controller;
 
 import java.util.Arrays;
-import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.tea.application.entity.Item;
+
 import com.tea.application.service.ItemService;
 import com.tea.application.service.UserService;
 
@@ -31,13 +28,26 @@ public class SearchController {
     private HttpServletRequest request;
 
     @GetMapping("/search")
-    public ResponseEntity<HashSet<Item>> getSearchResults(@RequestParam String searchTerm){
+    public String getSearchResults(@RequestParam String searchTerm, Model model){
+
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("authenticated") != null && (boolean) session.getAttribute("authenticated")) {
+            
+            if (session.getAttribute("userId") != null) {
+       
+                String userId= (String) session.getAttribute("userId");
+
+                model.addAttribute("userId", userId);
+            }
+        }
 
         String[] words = searchTerm.split("\\s+");
 
         Arrays.stream(words).filter(word -> !word.isEmpty()).toArray(String[]::new);
 
-        return new ResponseEntity<>(itemService.searchArray(words), HttpStatus.OK);
+        model.addAttribute("searchResults", itemService.searchArray(words));
+
+        return "home";
     }
 
     @GetMapping("/home")
